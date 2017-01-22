13268a707b74c796eeb404bb43fe2aa7ddd2e452
S.j. Sakib <sjsakib.bd@gmail.com>
2017-01-23 01:02:54 +0600

    Working state of verdicts

--- js/single.js
+++ js/single.js
@@ -5,13 +5,18 @@ $handle = "";
 $(document).ready(function() {
     $("#handleform").submit(function(e) {
         e.preventDefault();
-        console.log("submitted");
+        $(".handle-spinner").addClass("is-active");
         handle = $("#handle").val();
         $.get(api_url+"user.info", { "handles": handle }, function(data, status) {
+            $(".handle-spinner").removeClass("is-active");
             $(".chart-card").removeClass("hidden");
+            $(".chart-holder").empty();
+            $(".loading-spinner").removeClass("hidden");
+            $(".loading-spinner").addClass("is-active");
             google.charts.load('current', {'packages':['corechart']});
             google.charts.setOnLoadCallback(drawCharts);
         }).fail(function () {
+            $(".handle-spinner").removeClass("is-active");
             console.log("Couldn't connect to server or user does not exists");
             $("#handleDiv").addClass("is-invalid");
         });
@@ -47,8 +52,30 @@ function drawCharts() {
             else verdicts[sub.verdict]++;
         });
         var verTable = [["Verdict", "Count"]];
+        var sliceColors = [];
         for(ver in verdicts) {
-            verTable.push([ver,verdicts[ver]]);
+            if(ver == "OK") {
+                verTable.push(["AC",verdicts[ver]]);
+                sliceColors.push({color: 'green'})
+            } else if(ver == "WRONG_ANSWER") {
+                verTable.push(["WA",verdicts[ver]]);
+                sliceColors.push({color: 'red'})
+            } else if(ver == "TIME_LIMIT_EXCEEDED") {
+                verTable.push(["TLE",verdicts[ver]]);
+                sliceColors.push({color: 'blue'})
+            } else if(ver == "MEMORY_LIMIT_EXCEEDED") {
+                verTable.push(["MLE",verdicts[ver]]);
+                sliceColors.push({color: 'pink'})
+            } else if(ver == "RUNTIME_ERROR") {
+                verTable.push(["RTE",verdicts[ver]]);
+                sliceColors.push({color: 'purple'})
+            } else if(ver == "COMPILATION_ERROR") {
+                verTable.push(["CPE",verdicts[ver]]);
+                sliceColors.push({color: 'gray'})
+            } else  {
+                verTable.push([ver,verdicts[ver]]);
+                sliceColors.push({});
+            }
         }
         console.log(verTable);
         verdicts = new google.visualization.arrayToDataTable(verTable);
@@ -56,9 +83,17 @@ function drawCharts() {
         var options = {
           title: 'Verdicts',
           legend: 'none',
-          piSliceText: 'label'
+          pieSliceText: 'label',
+          slices: sliceColors,
+          fontName: 'Roboto',
+          titleTextStyle: {
+            fontSize: 18,
+            bold: false
+          }
         };
         var chart = new google.visualization.PieChart(document.getElementById('verdicts'));
         chart.draw(verdicts,options);
+        $("#verdictsSpinner").removeClass("is-active");
+        $("#verdictsSpinner").addClass("hidden");
     });
 }
