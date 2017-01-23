1f1f58d6163c7b39e75860a72f7689cad8b46415
S.j. Sakib <sjsakib.bd@gmail.com>
2017-01-23 15:22:25 +0600

    google chart loading moved

--- js/single.js
+++ js/single.js
@@ -1,20 +1,27 @@
 var api_url = "http://codeforces.com/api/";
 $handle = "";
 
+google.charts.load('current', {'packages':['corechart']});
+
 
 $(document).ready(function() {
     $("#handleform").submit(function(e) {
         e.preventDefault();
         $(".handle-spinner").addClass("is-active");
         handle = $("#handle").val();
+
         $.get(api_url+"user.info", { "handles": handle }, function(data, status) {
             $(".handle-spinner").removeClass("is-active");
             $(".chart-card").removeClass("hidden");
             $(".chart-holder").empty();
             $(".loading-spinner").removeClass("hidden");
             $(".loading-spinner").addClass("is-active");
-            google.charts.load('current', {'packages':['corechart']});
-            google.charts.setOnLoadCallback(drawCharts);
+
+            if(typeof google.visualization === 'undefined') {
+                google.charts.setOnLoadCallback(drawCharts);
+            } else {
+                drawCharts();
+            }
         }).fail(function () {
             $(".handle-spinner").removeClass("is-active");
             console.log("Couldn't connect to server or user does not exists");
@@ -28,25 +35,6 @@ function drawCharts() {
     $.get(api_url+"user.status", {"handle": handle}, function(data, status) {
         console.log(data);
         var verdicts = {};
-        /*verdicts = {
-            "Failed": 0,
-            "AC": 0,
-            "Partial": 0,
-            "CE": 0,
-            "RTE": 0,
-            "WA": 0,
-            "PE": 0,
-            "TLE": 0,
-            "MLE": 0,
-            "ILE": 0,
-            "SV": 0,
-            "Crashed": 0,
-            "IPC": 0,
-            "Challenged": 0,
-            "Skipped": 0,
-            "Testing": 0,
-            "Rejected": 0
-        };*/
         data.result.forEach(function(sub) {
             if(verdicts[sub.verdict] == undefined) verdicts[sub.verdict] = 1;
             else verdicts[sub.verdict]++;
@@ -77,9 +65,7 @@ function drawCharts() {
                 sliceColors.push({});
             }
         }
-        console.log(verTable);
         verdicts = new google.visualization.arrayToDataTable(verTable);
-        console.log(verdicts);
         var options = {
           title: 'Verdicts',
           legend: 'none',
@@ -89,7 +75,8 @@ function drawCharts() {
           titleTextStyle: {
             fontSize: 18,
             bold: false
-          }
+          },
+          is3D: true
         };
         var chart = new google.visualization.PieChart(document.getElementById('verdicts'));
         chart.draw(verdicts,options);
