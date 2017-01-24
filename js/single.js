3285e0ec85134043c0398fde0fe9836967c122a8
S.j. Sakib <sjsakib.bd@gmail.com>
2017-01-24 22:59:01 +0600

    everything looks cool

--- js/single.js
+++ js/single.js
@@ -1,17 +1,25 @@
 var api_url = "http://codeforces.com/api/";
+//var api_url = "/mnt/BAC4BB93C4BB4FFD/web/cfviz/";
 $handle = "";
 
 google.charts.load('current', { 'packages': ['corechart'] });
 
+function resetData() {
+  $("#mainSpinner").addClass("is-active");
+  $(".chart-card")
+    .empty()
+    .addClass("hidden");
+  $(".num-card").addClass("hidden");
+  $("#unsolvedList").empty();
+
+}
 
 $(document).ready(function() {
   $("#handleform").submit(function(e) {
     e.preventDefault();
-    $("#mainSpinner").addClass("is-active");
-    $(".chart-card")
-      .empty()
-      .addClass("hidden");
 
+    resetData();
+    
     handle = $("#handle").val();
 
     if (typeof google.visualization === 'undefined') {
@@ -24,6 +32,7 @@ $(document).ready(function() {
 });
 
 function drawCharts() {
+  //TODO: remove json
   $.get(api_url + "user.status", { "handle": handle }, function(data, status) {
     console.log(data);
 
@@ -33,8 +42,20 @@ function drawCharts() {
     var verdicts = {};
     var langs = {};
     var tags = {};
+    var levels = {};
+    var problems = {};
 
     data.result.forEach(function(sub) {
+      var problemId = sub.problem.contestId+'-'+sub.problem.index;
+      if(problems[problemId] === undefined) {
+        problems[problemId] = {
+          attempts: 1,
+          solved: false,
+        };
+      } else {
+        problems[problemId].attempts++;
+      }
+
       if (verdicts[sub.verdict] === undefined) verdicts[sub.verdict] = 1;
       else verdicts[sub.verdict]++;
 
@@ -46,6 +67,12 @@ function drawCharts() {
           if (tags[t] === undefined) tags[t] = 1;
           else tags[t]++;
         });
+
+        if(levels[sub.problem.index] === undefined) levels[sub.problem.index] = 1;
+        else levels[sub.problem.index]++;
+
+        problems[problemId].solved = true;
+
       }
     });
 
@@ -92,6 +119,7 @@ function drawCharts() {
       },
       is3D: true
     };
+    //$("#verdicts").height($("#verdicts").width());
     var verChart = new google.visualization.PieChart(document.getElementById('verdicts'));
     verChart.draw(verdicts, verOptions);
     $("#verdictsSpinner").removeClass("is-active");
@@ -117,9 +145,91 @@ function drawCharts() {
       },
       is3D: true
     };
+    //$("#langs").height($("#langs").width());
     langs = new google.visualization.arrayToDataTable(langTable);
     var langChart = new google.visualization.PieChart(document.getElementById('langs'));
     langChart.draw(langs, langOptions);
+
+
+    //the tags chart
+    var tagTable = [['Tag', 'Count']];
+    for(var tag in tags) {
+      tagTable.push([tag+": "+tags[tag], tags[tag]]);
+    }
+    var tagOptions = {
+      title: 'Tags',
+      pieSliceText: 'none',
+      legend: {
+        position: 'bottom',
+        maxLines: 2,
+        fontName: 'Roboto',
+      },
+      pieHole: 0.5,
+      tooltip: {
+        ignoreBounds: true,
+        text: 'value'
+      },
+      fontName: 'Roboto',
+      titleTextStyle: {
+        fontSize: 18,
+        bold: false
+      },
+    };
+    $("#tags").height($("#tags").width());
+    tags = new google.visualization.arrayToDataTable(tagTable);
+    var tagChart = new google.visualization.PieChart(document.getElementById('tags'));
+    tagChart.draw(tags, tagOptions);
+
+    //Plotting levels
+    var levelTable = [
+      ['Level', 'Count']
+    ];
+    for (var level in levels) {
+      levelTable.push([level, levels[level]]);
+    }
+    levelTable.sort(function(a,b) {
+      if(a[0] == 'Level') return 0;
+      if(a[0] > b[0]) return -1;
+      else return 1;
+    });
+    var levelOptions = {
+      title: 'Levels (Index in contest)',
+      legend: 'none',
+      fontName: 'Roboto',
+      titleTextStyle: {
+        fontSize: 18,
+        bold: false
+      },
+      hAxis: {format: '0'},
+    };
+    levels = new google.visualization.arrayToDataTable(levelTable);
+    var levelChart = new google.visualization.BarChart(document.getElementById('levels'));
+    levelChart.draw(levels, levelOptions);
+
+    //The numbers
+    var tried = 0;
+    var solved = 0;
+    var maxAttempt = 0;
+    var unsolved = [];
+    for(var p in problems) {
+      tried++;
+      if(problems[p].solved) solved++;
+      maxAttempt = Math.max(maxAttempt,problems[p].attempts);
+      if(!problems[p].solved) unsolved.push(p);
+    }
+    $(".num-card").removeClass("hidden");
+    $("#tried").html(tried);
+    $("#solved").html(solved);
+    $("#maxAttempt").html(maxAttempt);
+
+    unsolved.forEach(function(p) {
+      var con = p.split('-')[0];
+      var index = p.split('-')[1];
+      var url = "";
+      if(con.length < 4) url = "http://codeforces.com/contest/"+con+"/problem/"+index;
+      else url = "http://codeforces.com/problemset/gymProblem/"+con+"/"+index;
+      $("#unsolvedList").append("<div><a href=\""+url+"\" target=\"_blank\" class=\"lnk\">"+p+"</a></div>");
+    });
   })
   .fail(function() {
     $("#handleDiv").addClass("is-invalid");
