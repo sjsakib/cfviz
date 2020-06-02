2b65a035eadf8c5663db258a41db14bf2907f235
sjsakib <sjsakib.bd@gmail.com>
2020-06-02 22:51:51 +0600

    removed bmc badge

--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@ importScripts(
 );
 
 workbox.precaching.precacheAndRoute([
-  { url: '/index.html', revision: '1117' },
-  { url: '/about.html', revision: '1113' },
-  { url: '/compare.html', revision: '1113' },
-  { url: '/virtual-rating-change.html', revision: '1115' },
+  { url: '/index.html', revision: '1118' },
+  { url: '/about.html', revision: '1114' },
+  { url: '/compare.html', revision: '1114' },
+  { url: '/virtual-rating-change.html', revision: '1116' },
   { url: '/js/compare_helper.js', revision: '1113' },
   { url: '/js/compare.js', revision: '1111' },
   { url: '/js/calculate.js', revision: '1112' },
   { url: '/js/single.js', revision: '1112' },
-  { url: '/js/vir.js', revision: '1113' }
+  { url: '/js/vir.js', revision: '1113' },
 ]);
 
 workbox.routing.registerRoute(
   /^((?!codeforces)(?!facebook)(?!analytics)(?!ads)(?!google).)*$/,
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'local'
+    cacheName: 'local',
   })
 );
